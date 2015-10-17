676b070f915e8c1f020ae8e1a35f2c3ac918579e
konrad <wudkowski@gmail.com>
2015-10-17 12:41:20 +0100

    added Player model

--- src/main/scala/model/Player.scala
+++ src/main/scala/model/Player.scala
@@ -1,5 +1,8 @@
 package model
 
-class Player {
+case class Insult(id: Int, value: String)
+case class Comeback(id: Int, value: String)
 
-}
+case class Player(knownInsults: List[Insult], knownComebacks: List[Comeback])
+
+
