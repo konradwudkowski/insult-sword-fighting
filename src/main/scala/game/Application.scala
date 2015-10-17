458bf64c80e4148db255eaa85b0d64cce827560b
konrad <wudkowski@gmail.com>
2015-10-17 15:28:48 +0100

    first fight!

--- src/main/scala/game/Application.scala
+++ src/main/scala/game/Application.scala
@@ -8,7 +8,11 @@ object Application extends App {
 
 
   Iterator.continually(new ConsoleReader().readLine("> ")).takeWhile(_ != "x").foreach {
-    case "s" => println("Starting")
+    case "s" => {
+      println("Starting game for new player")
+      g.fight()
+    }
+
     case "--config" => g.repo.entries.map(_.generalInsult).foreach(println)
     case _ => println("Unrecognised command, valida commands are: ...")
   }
--- src/main/scala/game/GameEngine.scala
+++ src/main/scala/game/GameEngine.scala
@@ -4,5 +4,27 @@ import model._
 
 class GameEngine {
   val repo = new Repo
+  def randomInt = scala.util.Random.nextInt(repo.entries.size)
+  def randomInsults(howMany: Int) = repo.entries.map(entry => Insult(entry.id, entry.generalInsult)).take(howMany) // make it actually random
+  def getComebackForInsult(insult: Insult) = repo.entries.filter( _.id == insult.id).map(entry => Comeback(insult.id,entry.comeback))
+
+  def generatePlayer(howMany: Int) = {
+    val insults = randomInsults(howMany)
+    new Player(insults, insults.flatMap(getComebackForInsult))
+  }
+
+  def generatePirate = generatePlayer(3)
+
+  var player: Player = generatePlayer(howMany = 2)
+  var pirate: Player = _
+
+  def fight() = {
+    pirate = generatePirate
+    println(pirate.knownInsults.map(_.value).take(1))
+    println("Select your comeback")
+    player.knownComebacks.foreach { c =>
+      println(s"[${c.id}]    ${c.value}")
+    }
+  }
 }
 
